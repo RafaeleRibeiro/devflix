e03c835417fb9d7c92017be0563cc8f5c0519fe7
RafaeleRibeiro <rafaeleribeiro212@gmail.com>
2023-08-18 14:53:13 -0300

    .

--- src/componentes/movieCard/movieCard.jsx
+++ src/componentes/movieCard/movieCard.jsx
@@ -1,25 +1,32 @@
+import MovieDesc from "../movieDescription/movieDescription";
 import styles from "./movieCard.module.css";
 
-const MovieCard = ({movies}) => {
-    const movie = movies
-    return(
-        <div className={styles.movie}>
-            <div>
-                <p>{movie.Year}</p>
-            </div>
-            <div>
-                <img src={movie.Poster !== "N/A"
-                 ? movie.Poster 
-                 : "https://via.placeholder.com/400"}
-                  alt={movie.Title} 
-                  />
-            </div>
-            <div>
-                <span>{movie.Type}</span>
-                <h3>{movie.Title}</h3>
-            </div>
+const MovieCard = ({ movies }) => {
+  const movie = movies;
+  return (
+    <>
+      <div className={styles.movie}>
+        <div>
+          <p>{movie.Year}</p>
         </div>
-    )
-}
+        <div>
+          <img
+            src={
+              movie.Poster !== "N/A"
+                ? movie.Poster
+                : "https://via.placeholder.com/400"
+            }
+            alt={movie.Title}
+          />
+        </div>
+        <div>
+          <span>{movie.Type}</span>
+          <h3>{movie.Title}</h3>
+        </div>
+      </div>
+      <MovieDesc />
+    </>
+  );
+};
 
 export default MovieCard;
